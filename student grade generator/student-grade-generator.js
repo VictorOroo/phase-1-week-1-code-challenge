ca211590077551a71b4aeb08d36254981deed40f
victoronyonkaoroo <victor.oroo@student.moringaschool.com>
2023-06-17 21:10:10 +0300

    saturday commit

--- student grade generator/student-grade-generator.js
+++ student grade generator/student-grade-generator.js
@@ -1,17 +1,20 @@
-let marks = prompt ("Enter your grade:")
+function findGrade() {
+    let marks = parseFloat(prompt("Enter your grade:"));
 
-function findGrade(marks) {
-    if(marks >= 0 && marks <= 100){
-        if(marks > 79) {
-            return 'A';
-        }else if(marks >= 60 && marks <= 79){
-            return 'B';
-        }else if (marks >=50 && marks <= 59) {
-            return 'C';
-            
-        }else if (marks >= 40 && marks <=49){
-            return 'D'
-        }
+    if (marks < 0 || marks > 100) {
+        return 'invalid';
+        
+    } else if (marks > 79) {
+        return 'A';
+
+    }else if(marks >= 60 && marks <= 79){
+        return 'B';
+    }else if(marks >= 50 && marks <= 59){
+        return 'C';
+    }else if (marks >= 40 && marks <= 49) {
+        return 'D';
+        
+    }else{
+        return 'E';
     }
-    
 }
